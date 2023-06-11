1ba581733351ff37a026d36725fc051459d7d3ce
EMRE ŞAHBUDAK <118505939+EmreSahbudak@users.noreply.github.com>
2023-06-12 02:30:13 +0300

    admin login register için cross origin eklendi

--- AuthService/src/main/java/com/bilgeadam/constants/ApiUrls.java
+++ AuthService/src/main/java/com/bilgeadam/constants/ApiUrls.java
@@ -8,6 +8,7 @@ public class ApiUrls {
     public static final String REGISTER_MANAGER = "/register-manager";
     public static final String FIND_ALL="/find-all";
     public static final String LOGIN = "/login";
+    public static final String LOGIN_ADMIN = "/login-admin";
     public static final String UPDATE = "/update";
     public static final String DELETE_BY_ID = "/delete-by-id";
     public static final String FIND_BY_ID = "/find-by-id";
